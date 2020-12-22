24f6293e8cbac26303562d492be0331a222121ba
ryanfwu <32377569+ryanfwu@users.noreply.github.com>
2020-12-22 18:49:04 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("singleplayer").onclick = function playSingleplayer() {
+    window.location.href = "./singleplayer";
+}
+
+document.getElementById("multiplayer").onclick = function playMultiplayer() {
+    window.location.href = "./multiplayer";
+}
