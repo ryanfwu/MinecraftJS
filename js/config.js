ac0f95126ae2cc599860c81e6ff57a463e70415e
ryanfwu <32377569+ryanfwu@users.noreply.github.com>
2020-12-26 12:35:14 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 blockCodes = {
-    air: 0;
-    stone: 1;
-    grass: 2;
-    dirt: 3;
-    cobblestone: 4;
-    planks: 5;
-    log: 6;
-    bedrock: 7;
-}
+    air: 0,
+    stone: 1,
+    grass: 2,
+    dirt: 3,
+    cobblestone: 4,
+    planks: 5,
+    log: 6,
+    bedrock: 7
+};
