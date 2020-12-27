063c29b74eb2262032c2953c9d34b7e54b680b1c
ryanfwu <32377569+ryanfwu@users.noreply.github.com>
2020-12-27 12:57:38 -0500

    Update worlds.js

--- js/worlds.js
+++ js/worlds.js
@@ -1,10 +1,5 @@
-document.getElementById("seed").value = Math.round(Math.random()*65535)+1;
+document.getElementById("seed").value = Math.round(Math.random()*9223372036854775807)+1;
 document.getElementById("play").onclick = function() {
     var seed = document.getElementById("seed").value;
-    if (seed > 65535 || seed < 1) {
-        alert("Please enter an integer between 1 and 65535 inclusive for the seed.");
-    }
-    else {
-        window.location.href = "./singleplayer?seed=" + seed;
-    }
+    window.location.href = "./singleplayer?seed=" + seed;
 }
